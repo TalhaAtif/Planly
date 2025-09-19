64f5bcc1429c8897d3b6cade871c913886914380
_tvd <talha.atif.89@gmail.com>
2025-09-18 22:04:38 -0400

    Set up vite to work with docker, fixed errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,10 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import CourseManager from "./CourseManager";
 import "./App.css";
 
 function App() {
-  const [courses, setCourses] = useState([]);
-
-  const fetchCourses = async () => {
-    try {
-      const res = await axios.get("/api/courses");
-      setCourses(res.data);
-    } catch (err) {
-      console.error("Error fetching courses", err);
-    }
-  };
-
-  useEffect(() => {
-    fetchCourses();
-  }, []);
-
   return (
     <div className="App">
-      <h1>Courses</h1>
-      <pre>{JSON.stringify(courses, null, 2)}</pre>
+      <CourseManager />
     </div>
   );
 }
